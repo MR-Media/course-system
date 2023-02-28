478bcd4d2f7925fd89ae5bccca436ff47ac0dfce
Robinofskii <rvandenberg@yieldcomputer.com>
2023-02-28 17:47:38 +0100

    feat: :sparkles: add creation and deletion of quizzes to api

--- app/routes/quiz.ts
+++ app/routes/quiz.ts
@@ -2,8 +2,14 @@ import { Router } from "express";
 
 import { isAuthorized } from "../middleware/auth";
 
-import { get_quizzes } from "../controllers/quizController";
+import {
+  delete_quiz,
+  get_quizzes,
+  post_quiz,
+} from "../controllers/quizController";
 
 export const quizRouter = Router();
 
 quizRouter.get("/", isAuthorized, get_quizzes);
+quizRouter.post("/", isAuthorized, post_quiz);
+quizRouter.delete("/", isAuthorized, delete_quiz);
